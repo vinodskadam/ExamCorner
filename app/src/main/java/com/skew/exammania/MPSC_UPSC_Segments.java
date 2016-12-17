df8c15752e1caa6c891b42bf611e981a2660bc77
vinod kadam <vinod.s.kadam@gmail.com>
2016-12-17 17:51:43 +0530

    New Screens added

--- app/src/main/java/com/skew/exammania/MPSC_UPSC_Segments.java
+++ app/src/main/java/com/skew/exammania/MPSC_UPSC_Segments.java
@@ -3,11 +3,11 @@ package com.skew.exammania;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class MPSC_UPSC_Segments extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_mpsc__upsc__segments);
     }
 }
--- app/src/main/java/com/skew/exammania/WelcomeActivity.java
+++ app/src/main/java/com/skew/exammania/WelcomeActivity.java
@@ -3,11 +3,11 @@ package com.skew.exammania;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_welcome);
     }
 }
